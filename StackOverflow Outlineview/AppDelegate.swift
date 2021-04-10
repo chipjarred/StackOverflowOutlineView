5c37f93c0145a0cc239243665d2a52efe964307a
Chip Jarred <chipjarred@gmail.com>
2021-04-09 23:35:53 -0500

    Initial Commit

---
+++ StackOverflow Outlineview/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StackOverflow Outlineview
+//
+//  Created by Chip Jarred on 4/9/21.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StackOverflow Outlineview/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StackOverflow Outlineview
+//
+//  Created by Chip Jarred on 4/9/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
